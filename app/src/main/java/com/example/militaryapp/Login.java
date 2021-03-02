c321ff0d4321412d49817682379e0d93b7b3b57a
rezon <molchanov-ke01@mail.ru>
2021-03-02 14:54:50 +0300

    build 1.3 #finish add ResetPassword, new icon, clean layouts

--- app/src/main/java/com/example/militaryapp/Login.java
+++ app/src/main/java/com/example/militaryapp/Login.java
@@ -1,21 +1,17 @@
 package com.example.militaryapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
@@ -23,6 +19,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     EditText et_email, et_password;
     Button btn_login, btn_register;
     TextView tv_forgot_pass, tv_guest;
+    ProgressBar progressBar;
 
     FirebaseAuth mAuth;
 
@@ -36,13 +33,14 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         et_email = (EditText) findViewById(R.id.email);
         et_password = (EditText) findViewById(R.id.password);
         tv_forgot_pass = (TextView) findViewById(R.id.tv_forgot_pass);
-        tv_forgot_pass.setOnClickListener(this);
-
         btn_register = (Button) findViewById(R.id.btn_register);
-        btn_register.setOnClickListener(this);
         btn_login = (Button) findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(this);
         tv_guest = (TextView) findViewById(R.id.tv_guest);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        tv_forgot_pass.setOnClickListener(this);
+        btn_register.setOnClickListener(this);
+        btn_login.setOnClickListener(this);
         tv_guest.setOnClickListener(this);
     }
 
@@ -58,6 +56,9 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             case R.id.btn_register:
                 startActivity(new Intent(this, Register.class));
                 break;
+            case R.id.tv_forgot_pass:
+                startActivity(new Intent(this, ResetPassword.class));
+                break;
             default:
                 break;
         }
@@ -65,7 +66,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     private void userLogin() {
         String email = et_email.getText().toString().trim();
-        String password = et_password.getText().toString().trim();
+        String password = this.et_password.getText().toString().trim();
 
         if (email.isEmpty()) {
             et_email.setError("Заполните поле!");
@@ -78,18 +79,18 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             return;
         }
         if (password.isEmpty()) {
-            et_password.setError("Заполните поле!");
-            et_password.requestFocus();
+            this.et_password.setError("Заполните поле!");
+            this.et_password.requestFocus();
             return;
         }
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    startActivity(new Intent(Login.this, MainActivity.class));
-                } else {
-                    Toast.makeText(Login.this, "Такого пользователя не существует!", Toast.LENGTH_LONG).show();
-                }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+
+            if (task.isSuccessful()) {
+                progressBar.setVisibility(View.GONE);
+                startActivity(new Intent(Login.this, MainActivity.class));
+            } else {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(Login.this, "Такого пользователя не существует!", Toast.LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/com/example/militaryapp/Register.java
+++ app/src/main/java/com/example/militaryapp/Register.java
@@ -3,13 +3,12 @@ package com.example.militaryapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,12 +17,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.regex.Pattern;
+public class Register extends AppCompatActivity{
 
-public class Register extends AppCompatActivity implements View.OnClickListener {
-
-    EditText r_login, r_email, r_password;
+    EditText et_login, et_email, et_password;
     Button btn_register;
+    ProgressBar progressBar;
 
     FirebaseAuth mAuth;
 
@@ -34,76 +32,66 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
         mAuth = FirebaseAuth.getInstance();
 
-        r_login = (EditText) findViewById(R.id.r_login);
-        r_email = (EditText) findViewById(R.id.r_email);
-        r_password = (EditText) findViewById(R.id.r_password);
+        et_login = (EditText) findViewById(R.id.r_login);
+        et_email = (EditText) findViewById(R.id.r_email);
+        et_password = (EditText) findViewById(R.id.r_password);
         btn_register = (Button) findViewById(R.id.r_btn_register);
-        btn_register.setOnClickListener(this);
-    }
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.r_btn_register:
-                registerUser();
-                break;
-            default:
-                break;
-        }
+        btn_register.setOnClickListener(v -> registerUser());
     }
 
     private void registerUser() {
-        String login = r_login.getText().toString().trim();
-        String email = r_email.getText().toString().trim();
-        String password = r_password.getText().toString().trim();
+        String login = et_login.getText().toString().trim();
+        String email = et_email.getText().toString().trim();
+        String password = et_password.getText().toString().trim();
 
         if (login.isEmpty()) {
-            r_login.setError("Заполните поле!");
-            r_login.requestFocus();
+            et_login.setError("Заполните поле!");
+            et_login.requestFocus();
             return;
         }
         if (email.isEmpty()) {
-            r_email.setError("Заполните поле!");
-            r_email.requestFocus();
+            et_email.setError("Заполните поле!");
+            et_email.requestFocus();
             return;
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            r_email.setError("Введите корректную почту!");
-            r_email.requestFocus();
+            et_email.setError("Введите корректную почту!");
+            et_email.requestFocus();
             return;
         }
         if (password.isEmpty()) {
-            r_password.setError("Заполните поле!");
-            r_password.requestFocus();
+            et_password.setError("Заполните поле!");
+            et_password.requestFocus();
             return;
         }
         if (password.length() < 6) {
-            r_password.setError("Минимальная длина пароль 6 символов!");
-            r_password.requestFocus();
+            et_password.setError("Минимальная длина пароль 6 символов!");
+            et_password.requestFocus();
             return;
         }
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            User user = new User(login, email, password);
 
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    progressBar.setVisibility(View.VISIBLE);
+                    if (task.isSuccessful()) {
+                        User user = new User(login, email, password);
+
+                        FirebaseDatabase.getInstance().getReference("Users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user).addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
                                         Toast.makeText(Register.this, "Регистрация завершена!", Toast.LENGTH_LONG).show();
                                         //переход в Login layout
                                     } else {
                                         Toast.makeText(Register.this, "Ошибка! Попробуйте ещё раз!", Toast.LENGTH_LONG).show();
                                     }
-                                }
-                            });
-                        } else {
-                            Toast.makeText(Register.this, "Ошибка! Попробуйте ещё раз!", Toast.LENGTH_LONG).show();
-                        }
+                            progressBar.setVisibility(View.GONE);
+                        });
+                    } else {
+                        Toast.makeText(Register.this, "Ошибка! Попробуйте ещё раз!", Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.GONE);
                     }
                 });
     }
